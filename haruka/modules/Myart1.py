f542bcb3f11e28ab51a2b85093f35ee5a678b7ed
wiza425 <69521271+wiza425@users.noreply.github.com>
2020-10-01 14:02:38 +0530

    Add files via upload

---
+++ haruka/modules/Myart1.py
@@ -0,0 +1,113 @@
+import random
+from telegram.ext import run_async, Filters
+from telegram import Message, Chat, Update, Bot, MessageEntity
+from haruka import dispatcher
+from haruka.modules.disable import DisableAbleCommandHandler
+
+ART_STRINGS = (
+"░░░░▓\n"
+"░░░▓▓\n"
+"░░█▓▓█\n"
+"░██▓▓██\n"
+"░░██▓▓██\n"
+"░░░██▓▓██\n"
+"░░░░██▓▓██\n"
+"░░░░░██▓▓██\n"
+"░░░░██▓▓██\n"
+"░░░██▓▓██\n"
+"░░██▓▓██\n"
+"░██▓▓██\n"
+"░░██▓▓██\n"
+"░░░██▓▓██\n"
+"░░░░██▓▓██\n"
+"░░░░░██▓▓██\n"
+"░░░░██▓▓██\n"
+"░░░██▓▓██\n"
+"░░██▓▓██\n"
+"░██▓▓██\n"
+"░░██▓▓██\n"
+"░░░██▓▓██\n"
+"░░░░██▓▓██\n"
+"░░░░░██▓▓██\n"
+"░░░░██▓▓██\n"
+"░░░██▓▓██\n"
+"░░██▓▓██\n"
+"░██▓▓██\n"
+"░░██▓▓██\n"
+"░░░██▓▓██\n"
+"░░░░██▓▓██\n"
+"░░░░░██▓▓██\n"
+"░░░░██▓▓██\n"
+"░░░██▓▓██\n"
+"░░██▓▓██\n"
+"░██▓▓██\n"
+"░░██▓▓██\n"
+"░░░██▓▓██\n"
+"░░░░██▓▓██\n"
+"░░░░░██▓▓██\n"
+"░░░░██▓▓██\n"
+"░░░██▓▓██\n"
+"░░██▓▓██\n"
+"░██▓▓██\n"
+"░░██▓▓██\n"
+"░░░██▓▓██\n"
+"░░░░██▓▓██\n"
+"░░░░░██▓▓██\n"
+"░░░░██▓▓██\n"
+"░░░██▓▓██\n"
+"░░██▓▓██\n"
+"░██▓▓██\n"
+"░░██▓▓██\n"
+"░░░██▓▓██\n"
+"░░░░██▓▓██\n"
+"░░░░░██▓▓██\n"
+"░░░░██▓▓██\n"
+"░░░██▓▓██\n"
+"░░██▓▓██\n"
+"░██▓▓██\n"
+"░░██▓▓██\n"
+"░░░██▓▓██\n"
+"░░░░██▓▓██\n"
+"░░░░░██▓▓██\n"
+"░░░░██▓▓██\n"
+"░░░██▓▓██\n"
+"░░██▓▓██\n"
+"░░██▓▓██\n"
+"░░██▓▓██\n"
+"░░██▓▓██\n"
+"░░██▓▓██\n"
+"░░██▓▓██\n"
+"░░░██▓▓███\n"
+"░░░░██▓▓████\n"
+"░░░░░██▓▓█████\n"
+"░░░░░░██▓▓██████\n"
+"░░░░░░███▓▓███████\n"
+"░░░░░████▓▓████████\n"
+"░░░░█████▓▓█████████\n"
+"░░░█████░░░█████●███\n"
+"░░████░░░░░░░███████\n"
+"░░███░░░░░░░░░██████\n"
+"░░██░@WrEnChz░████\n"
+"░░░░░░░░░░░░░░░░███\n"
+"░░░░░░░░░░░░░░░░░░░\n")
+
+ 
+@run_async
+def art(bot: Bot, update: Update):
+    bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
+    message = update.effective_message
+    if message.reply_to_message:
+      message.reply_to_message.reply_text(random.choice(ART_STRINGS))
+    else:
+      message.reply_text(random.choice(ART_STRINGS))
+
+
+__help__ = """
+- /art : Get some fantastuc Arts.
+"""
+
+__mod_name__ = "ART"
+
+ART_HANDLER = DisableAbleCommandHandler("art", art)
+
+dispatcher.add_handler(ART_HANDLER)
