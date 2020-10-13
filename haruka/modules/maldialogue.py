950dc33c9e90393f191424a538e96bbf2c68e19d
wiza425 <69521271+wiza425@users.noreply.github.com>
2020-10-13 19:28:31 +0530

    Update maldialogue.py

--- haruka/modules/maldialogue.py
+++ haruka/modules/maldialogue.py
@@ -140,11 +140,11 @@ VERYSHAME_HANDLER = DisableAbleCommandHandler("veryshame", veryshame)
 
 
 
-dispatcher
-add_handler(DIALOGUE_HANDLER)
-dispatcher
-add_handler(PROPOSE_HANDLER)
-dispatcher
-add_handler(SHAME_HANDLER)
-dispatcher
-add_handler(VERYSHAME_HANDLER)
+
+dispatcher.add_handler(DIALOGUE_HANDLER)
+
+dispatcher.add_handler(PROPOSE_HANDLER)
+
+dispatcher.add_handler(SHAME_HANDLER)
+
+dispatcher.add_handler(VERYSHAME_HANDLER)
